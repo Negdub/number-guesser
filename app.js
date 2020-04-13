37d2d9ba9ca52e4259ce468b1ab3f06a0ecd239c
jordan budgen <jordanbudgen@gmail.com>
2020-04-13 11:17:22 -0700

    added lose case and game over

--- app.js
+++ app.js
@@ -7,47 +7,76 @@ GAME FUNCTION:
 - Let player choose to play again
 */
 
-// game values
+// Game values
 let min = 1,
     max = 10,
     winningNum = 2,
     guessesLeft = 3;
 
-// ui elements
+// UI Elements
 const game = document.querySelector('#game'),
       minNum = document.querySelector('.min-num'),
       maxNum = document.querySelector('.max-num'),
-      guessInput = document.querySelector('#guess-input'),
       guessBtn = document.querySelector('#guess-btn'),
+      guessInput = document.querySelector('#guess-input'),
       message = document.querySelector('.message');
 
-//  Assign Ui min and max
+// Assign UI min and max
 minNum.textContent = min;
 maxNum.textContent = max;
-
-// addeventlistener to guessBTN
+      
+// Listen for guess
 guessBtn.addEventListener('click', function(){
   let guess = parseInt(guessInput.value);
-
-  // validate
+  
+  // Validate
   if(isNaN(guess) || guess < min || guess > max){
     setMessage(`Please enter a number between ${min} and ${max}`, 'red');
   }
 
-  // check if won
+  // Check if won
   if(guess === winningNum){
-    // disable input
-    guessInput.disabled = true;
-    // change border color
-    guessInput.style.borderColor = 'green';
-    // set message
-    setMessage(`${winningNum} is correct! Nice!`, 'green');
-  } else {
+    // Game over - won
+    gameOver(true, `${winningNum} is correct, YOU WIN!`);
 
+  } else {
+    // Wrong number
+    guessesLeft -= 1;
+
+    if(guessesLeft === 0){
+      // Game over - lost
+      gameOver(false, `Game Over, you lost. The correct number was ${winningNum}`);
+    } else {
+      // Game continues - answer wrong
+
+      // Change border color
+      guessInput.style.borderColor = 'red';
+
+      // Clear Input
+      guessInput.value = '';
+
+      // Tell user its the wrong number
+      setMessage(`${guess} is not correct, ${guessesLeft} guesses left`, 'red');
+    }
   }
 });
 
-// set message
+// Game over
+function gameOver(won, msg){
+  let color;
+  won === true ? color = 'green' : color = 'red';
+
+  // Disable input
+  guessInput.disabled = true;
+  // Change border color
+  guessInput.style.borderColor = color;
+  // Set text color
+  message.style.color = color;
+  // Set message
+  setMessage(msg);
+}
+
+// Set message
 function setMessage(msg, color){
   message.style.color = color;
   message.textContent = msg;
